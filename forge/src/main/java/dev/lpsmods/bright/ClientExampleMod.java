3c4c1c07f1d218e0f5c3f4f8aa5f526588041203
Legopitstop <1589lego@gmail.com>
2025-02-20 16:10:11 -0600

    Initial release

--- forge/src/main/java/dev/lpsmods/bright/ClientExampleMod.java
+++
@@ -1,7 +0,0 @@
-package dev.lpsmods.bright;
-
-public class ClientExampleMod {
-    public static void init() {
-        ClientBootstrap.init();
-    }
-}
--- forge/src/main/java/dev/lpsmods/bright/platform/ForgePlatformHelper.java
+++ forge/src/main/java/dev/lpsmods/bright/platform/ForgePlatformHelper.java
@@ -4,6 +4,9 @@ import dev.lpsmods.bright.platform.services.IPlatformHelper;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.loading.FMLLoader;
 
+/**
+ * <a href="https://docs.minecraftforge.net/en/1.21.x/gettingstarted/">Forge Documentation</a>
+ * */
 public class ForgePlatformHelper implements IPlatformHelper {
 
     @Override
